641dc6ef64d987e7fbd815fcafdc538727757135
stefa <stefa@10.0.0.4>
2020-04-14 19:51:45 +0200

    version144

--- WebAppM/src/main/java/pck/BenutzerBean.java
+++ WebAppM/src/main/java/pck/BenutzerBean.java
@@ -1,14 +1,35 @@
 package pck;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.NoneScoped;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionListener;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.event.ValueChangeListener;
 
 @ManagedBean
-@SessionScoped
-public class BenutzerBean {
+@RequestScoped
+public class BenutzerBean implements ActionListener,ValueChangeListener{
 
-	private String username;
-	private String password;
+	private String username="";
+	private String password="";
+
+	
+	@ManagedProperty(value="#{dBManager}")
+	private DBManager db;
+	
+	
+	public DBManager getDb() {
+		return db;
+	}
+
+	public void setDb(DBManager db) {
+		this.db = db;
+	}
 
 	public String getUsername() {
 		return username;
@@ -26,4 +47,19 @@ public class BenutzerBean {
 		this.password = password;
 	}
 
+	public void processAction(ActionEvent event) throws AbortProcessingException {
+		System.out.println("User Submit "+db.getCount());
+		db.incrementCount();
+		
+	}
+
+	public void processValueChange(ValueChangeEvent event) throws AbortProcessingException {
+		System.out.println("Value User Submit "+db.getCount1());
+		db.incrementCount1();
+		
+	}
+
+	
+	
+	
 }
--- WebAppM/src/main/java/pck/DBManager.java
+++ WebAppM/src/main/java/pck/DBManager.java
@@ -1,5 +1,35 @@
 package pck;
 
-public class DBManager {
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
 
+@ManagedBean
+@ApplicationScoped
+public class DBManager {
+	
+	private int count = 0;
+	private int count1 = 0;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public void incrementCount(){
+		count++;
+	}
+	public void incrementCount1(){
+		count1++;
+	}
+	public int getCount1() {
+		return count1;
+	}
+
+	public void setCount1(int count1) {
+		this.count1 = count1;
+	}
+	
 }
